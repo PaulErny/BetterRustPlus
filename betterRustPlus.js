273f99e4a4fb4e6dba76af324497004251a4de9a
PaulErny <paul.erny@epitech.eu>
2024-04-01 18:08:55 +0200

    ADD - dynamic commands import and mapping

--- betterRustPlus.js
+++ betterRustPlus.js
@@ -1,11 +1,36 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Collection, Client, Events, GatewayIntentBits } from 'discord.js'
 import { config } from 'dotenv'
-import * as test from './commands/test.js'
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+import * as test from './commands/utility/test.js'
 
 config()
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.commands = new Collection()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const foldersPath = path.join(__dirname, 'commands')
+const commandFolder = fs.readdirSync(foldersPath)
+
+for(const folder of commandFolder) {
+	const commandsPath = path.join(foldersPath, folder)
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = await import(filePath)
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
 
 // A function executed when the Discord client receives an interaction
 async function handleInteraction(interaction) {
@@ -13,8 +38,22 @@ async function handleInteraction(interaction) {
 	if (!interaction.isCommand()) return;
   
 	// Command execution mapping
-	if (interaction.commandName === 'ping') {
-	  await test.execute(interaction);
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	}
 }
 
